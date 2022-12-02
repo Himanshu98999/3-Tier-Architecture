2a852d615bb184348ea85e07df38b172219fb669
Himanshu98999 <119611334+Himanshu98999@users.noreply.github.com>
2022-12-02 14:26:37 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,21 @@
+provider"google" {
+
+  project               ="ch-audience-admin-6f2f"
+
+  user_project_override = true
+
+  # to be replaced with service account key
+
+  credentials = "C:\\Users\\vk635920\\AppData\\Roaming\\gcloud\\application_default_credentials.json"
+
+}
+
+
+
+provider"google-beta" {
+
+  project               ="ch-audience-admin-6f2f"
+
+  user_project_override = true
+
+}
