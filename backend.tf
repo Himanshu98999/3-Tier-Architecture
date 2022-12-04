6bf707c62bc633093419fb3d99ab2f313116f20c
Himanshu98999 <119611334+Himanshu98999@users.noreply.github.com>
2022-12-04 20:55:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -27,3 +27,17 @@ module "mig2" {
   network    = google_compute_network.default.self_link
   subnetwork = google_compute_subnetwork.default.self_link
 }
+
+  module "gce-ilb" {
+  source       = "GoogleCloudPlatform/lb-internal/google"
+  version      = "~> 2.0"
+  region       = var.region
+  name         = "group2-ilb"
+  ports        = ["80"]
+  health_check = var.health_check
+  source_tags  = ["allow-group1"]
+  target_tags  = ["allow-group2", "allow-group3"]
+  backends     = [
+    { group = module.mig2.instance_group, description = "", failover = false },
+     ]
+}
