f4b9b31824c6ebcd44ef94b56370b71ca11383a6
Himanshu98999 <119611334+Himanshu98999@users.noreply.github.com>
2022-12-04 17:59:25 +0530

    Update database.tf

--- database.tf
+++ database.tf
@@ -1,4 +1,34 @@
-module "sql-db" {
-  source  = "GoogleCloudPlatform/sql-db/google//modules/mysql"
-  version = "8.0.0"
+# Handle Database
+resource "google_sql_database_instance" "main" {
+  name             = "${var.deployment_name}-db-${random_id.id.hex}"
+  database_version = "MYSQL_5_7"
+  region           = var.region
+  project          = var.project_id
+
+  settings {
+    tier                  = "db-g1-small"
+    disk_autoresize       = true
+    disk_autoresize_limit = 0
+    disk_size             = 10
+    disk_type             = "PD_SSD"
+    user_labels           = var.labels
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = module.network-safer-mysql-simple.network_self_link
+    }
+    location_preference {
+      zone = var.zone
+    }
+  }
+  deletion_protection = false
+  depends_on = [
+    module.project-services,
+    google_vpc_access_connector.main,
+  ]
+}
+
+resource "google_sql_database" "database" {
+  project  = var.project_id
+  name     = "todo"
+  instance = google_sql_database_instance.main.name
 }
